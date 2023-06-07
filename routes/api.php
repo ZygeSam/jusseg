b858cf9608b8fb2fb7e6c431b3dd0306b999710c
Sakinbarnes Samuel <samuel.sakinbarnes@sevenbridgesolutions.com>
2023-06-07 15:55:14 +0100

    added more routes for the air services

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AirController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,33 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+//---------------->Air
+Route::prefix('air')->group(function () {
+    //---------------->Airlines
+    Route::prefix('airlines')->group(function () {
+        Route::get('/', [AirController::class, 'index']);
+    });
+
+    //---------------->Airports
+    Route::prefix('airports')->group(function () {
+        Route::get('/', [AirController::class, 'getAirports']);
+        Route::get('/nearby', [AirController::class, 'getAirportsNearBy']);
+    });
+
+    //---------------->Flights
+    Route::prefix('flights')->group(function () {
+        Route::get('/rules', [AirController::class, 'displayFlightRules']);
+        Route::get('/meals', [AirController::class, 'flightMeals']);
+        Route::get('/upsell', [AirController::class, 'getAirUpSell']);
+        Route::get('/branded-fare', [AirController::class, 'getFare']);
+        Route::get('/roundtrip', [AirController::class, 'roundTrip']);
+        Route::get('/oneway', [AirController::class, 'oneWayTrip']);
+        Route::get('/multicity', [AirController::class, 'multiCityTrip']);
+        Route::get('/flexibletrip', [AirController::class, 'flexibleTrip']);
+        Route::get('/storeflight', [AirController::class, 'storeFlight']);
+    });
 });
