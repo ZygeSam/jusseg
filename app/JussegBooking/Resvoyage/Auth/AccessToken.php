e6e2a3134113646ef058813544e2596c6d8e3ee9
Sakinbarnes Samuel <samuel.sakinbarnes@sevenbridgesolutions.com>
2023-06-07 15:46:13 +0100

    added static token and expiry variables; modifiied the getToken method

--- app/JussegBooking/Resvoyage/Auth/AccessToken.php
+++ app/JussegBooking/Resvoyage/Auth/AccessToken.php
@@ -10,6 +10,8 @@ class AccessToken
 {
     use ApiResponseTrait;
     private $url;
+    private static $token;
+    private static $expiry;
     /**
      * Make the cURL call for accessing bearer token
      * Bearer token expires 300 minutes after created
@@ -18,16 +20,31 @@ class AccessToken
      */
     public function getToken()
     {
-        $this->url = config('booking.resvoyage.api_url') . "/public/token";
+        if (!isset(self::$token) || $this->isTokenExpired()) {
 
-        //get the token 
-        $token =
-            Http::acceptJson()->get($this->url, [
-                'clientname' => config('booking.resvoyage.clientname')
-            ]);
-        return $this->apiResponse($token);
+            $this->url = config('booking.resvoyage.api_url') . "/public/token";
 
-        //cache token
-        //refresh token after 300 minutes
+            //get the token 
+            $response =
+                Http::acceptJson()->get($this->url, [
+                    'clientname' => config('booking.resvoyage.clientname')
+                ]);
+            $token =  $this->apiResponse($response);
+
+            if ($token) {
+                self::$token = json_decode($token)->Token;
+                self::$expiry = time() + 300 * 60; // Set token expiry time to 300 seconds * 60  (5*60 minutes)
+            }
+        }
+        return self::$token;
+    }
+
+    /**
+     * Check if the token has expired
+     * @return bool
+     */
+    private function isTokenExpired()
+    {
+        return isset(self::$expiry) && self::$expiry <= time();
     }
 }
